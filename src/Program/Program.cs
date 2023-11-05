9ecf374a19c02db24f7aaf74c007564885d5a5d4
Lucía Amor <141635491+lu-amor@users.noreply.github.com>
2023-11-05 16:16:17 -0300

    Parte 2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,22 +7,35 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Node n1 = new Node(1);
-            Node n2 = new Node(2);
-            Node n3 = new Node(3);
-            Node n4 = new Node(4);
-            Node n5 = new Node(5);
-            Node n6 = new Node(6);
-            Node n7 = new Node(7);
+            Person enrique = new Person("Enrique", 87);
+            Person juan = new Person("Juan", 45);
+            Person pablo = new Person("Pablo", 52);
+            Person daniela = new Person("Daniela", 48);
+            Person martina = new Person("Martina", 5);
+            Person valeria = new Person("Valeria", 16);
+            Person olivia = new Person("Olivia", 15);
+            Person maria = new Person("María", 17);
+            Person diego = new Person("Diego", 12);
+            Node nEnrique = new Node(enrique);
+            Node nMartina = new Node(martina);
+            Node nJuan = new Node(juan);
+            Node nMaria = new Node(maria);
+            Node nPablo = new Node(pablo);
+            Node nDaniela = new Node(daniela);
+            Node nOlivia = new Node(olivia);
+            Node nValeria = new Node(valeria);
+            Node nDiego = new Node(diego);
 
-            n1.AddChildren(n2);
-            n1.AddChildren(n3);
+            nEnrique.AddChildren(nJuan);
+            nEnrique.AddChildren(nDaniela);
+            nEnrique.AddChildren(nPablo);
 
-            n2.AddChildren(n4);
-            n2.AddChildren(n5);
+            nJuan.AddChildren(nMartina);
+            nDaniela.AddChildren(nValeria);
 
-            n3.AddChildren(n6);
-            n3.AddChildren(n7);
+            nPablo.AddChildren(nDiego);
+            nPablo.AddChildren(nMaria);
+            nPablo.AddChildren(nOlivia);
 
             // visitar el árbol aquí
         }
